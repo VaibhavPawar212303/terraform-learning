158a2f3d0b49241560cc106a7132d3d177543658
VaibhavPawar212303 <pawarvaibhav.vppv@gmail.com>
2025-03-04 22:14:53 +0530

    added cred

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ provider "google" {
   project = "test-automation-445606"
   region  = "us-central1"
   zone    = "us-central1-c"
+  credentials = file("/home/pawarvaibhav_vppv/.config/gcloud/application_default_credentials.json")
 }
 
 resource "google_compute_network" "vpc_network" {
