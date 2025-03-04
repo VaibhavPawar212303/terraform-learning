afe1c39d99efd7bcfc143cdbb6d8a010528450e2
VaibhavPawar212303 <pawarvaibhav.vppv@gmail.com>
2025-03-04 22:43:02 +0530

    update file

--- main.tf
+++ main.tf
@@ -1,58 +1,49 @@
 terraform {
   required_providers {
     google = {
-      source  = "hashicorp/google"
-      version = "6.8.0"
+      source                = "hashicorp/google"
+      version               = "6.8.0"
     }
   }
 }
 
 provider "google" {
-  project = "test-automation-445606"
-  region  = "us-central1"
-  zone    = "us-central1-c"
-  credentials = file("/home/pawarvaibhav_vppv/.config/gcloud/application_default_credentials.json")
+  project                   = "test-automation-445606"
+  region                    = "us-central1"
+  zone                      = "us-central1-c"
+  credentials               = file("/home/pawarvaibhav_vppv/.config/gcloud/application_default_credentials.json")
 }
-
 
 resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
-  auto_create_subnetworks = true
+  name                      = "terraform-network"
+  auto_create_subnetworks   =  true
 }
-
 
 resource "google_compute_firewall" "allow_ssh" {
-  name    = "allow-ssh"
-  network = google_compute_network.vpc_network.name
-
+  name                      = "allow-ssh"
+  network                   = google_compute_network.vpc_network.name
   allow {
-    protocol = "tcp"
-    ports    = ["22"]
+    protocol                = "tcp"
+    ports                   = ["22"]
   }
-
-  source_ranges = ["0.0.0.0/0"] 
+  source_ranges             = ["0.0.0.0/0"] 
 }
 
-
 resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "f1-micro"
-
+  name                      = "terraform-instance"
+  machine_type              = "f1-micro"
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image                 = "debian-cloud/debian-11"
     }
   }
-
   network_interface {
-    network = google_compute_network.vpc_network.name
+    network                 = google_compute_network.vpc_network.name
     access_config {
     }
   }
-
-
   service_account {
-    email  = "default"
-    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+    email                   = "default"
+    scopes                  = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 }
