56bed5184531cb83c365498c71f304c0f59960d0
VaibhavPawar212303 <pawarvaibhav.vppv@gmail.com>
2025-03-04 21:43:11 +0530

    create VM

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "6.8.0"
     }
   }
@@ -14,8 +14,23 @@ provider "google" {
 }
 
 resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+  name                    = "terraform-network"
+  auto_create_subnetworks = true
 }
+
+
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"] 
+}
+
 
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
@@ -32,5 +47,9 @@ resource "google_compute_instance" "vm_instance" {
     access_config {
     }
   }
-}
 
+  service_account {
+    email  = "default"
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+}
