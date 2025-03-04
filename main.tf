c647700b5f4077212006c632e1624c7a6d1a4ce5
VaibhavPawar212303 <pawarvaibhav.vppv@gmail.com>
2025-03-04 20:53:15 +0530

    create VM in VPC

--- main.tf
+++ main.tf
@@ -8,7 +8,6 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("/home/pawarvaibhav_vppv/.config/gcloud/application_default_credentials.json")
   project = "test-automation-445606"
   region  = "us-central1"
   zone    = "us-central1-c"
@@ -17,3 +16,21 @@ provider "google" {
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
